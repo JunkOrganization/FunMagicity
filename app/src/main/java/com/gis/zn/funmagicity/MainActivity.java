8abe005b41294ceda6f4be4e637912f09f174779
Nora0713 <1379026619@qq.com>
2018-02-18 20:24:10 +0800

    init:map

--- app/src/main/java/com/gis/zn/funmagicity/MainActivity.java
+++ app/src/main/java/com/gis/zn/funmagicity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.gis.zn.funmagicity.entity.User;
 
 import cn.bmob.v3.Bmob;
@@ -11,10 +13,15 @@ import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
         //提供以下两种方式进行初始化操作：
@@ -49,5 +56,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mMapView = (MapView) findViewById(R.id.bmapView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
